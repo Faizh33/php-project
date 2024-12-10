9437b36325b0bf14be2aaa3abec4b23c0351dd14
Faizh33 <erika_redon@hotmail.com>
2024-12-10 22:41:48 +0100

    creation of function and category entities

---
+++ src/Entity/Category.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategoryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CategoryRepository::class)]
+class Category
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    /**
+     * @var Collection<int, PhpFunction>
+     */
+    #[ORM\OneToMany(targetEntity: PhpFunction::class, mappedBy: 'category')]
+    private Collection $phpFunction;
+
+    public function __construct()
+    {
+        $this->phpFunction = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, PhpFunction>
+     */
+    public function getPhpFunction(): Collection
+    {
+        return $this->phpFunction;
+    }
+
+    public function addPhpFunction(PhpFunction $phpFunction): static
+    {
+        if (!$this->phpFunction->contains($phpFunction)) {
+            $this->phpFunction->add($phpFunction);
+            $phpFunction->setCategory($this);
+        }
+
+        return $this;
+    }
+
+    public function removePhpFunction(PhpFunction $phpFunction): static
+    {
+        if ($this->phpFunction->removeElement($phpFunction)) {
+            // set the owning side to null (unless already changed)
+            if ($phpFunction->getCategory() === $this) {
+                $phpFunction->setCategory(null);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ src/Entity/PhpFunction.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PhpFunctionRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PhpFunctionRepository::class)]
+class PhpFunction
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $definition = null;
+
+    #[ORM\ManyToOne(inversedBy: 'phpFunction')]
+    private ?Category $category = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDefinition(): ?string
+    {
+        return $this->definition;
+    }
+
+    public function setDefinition(string $definition): static
+    {
+        $this->definition = $definition;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): static
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+}
