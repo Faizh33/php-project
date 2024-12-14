b7f77cedc8a6619bef3ae95e195dd43aca93c99f
Faizh33 <erika_redon@hotmail.com>
2024-12-14 16:30:28 +0100

    create functions list function

--- src/Controller/PhpFunctionController.php
+++ src/Controller/PhpFunctionController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\PhpFunction;
+use App\Repository\PhpFunctionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -12,5 +14,45 @@ class PhpFunctionController extends AbstractController
     public function index(): Response
     {
         return $this->render('php_function/index.html.twig');
+    }
+
+    #[Route('/php-functions/list', name: 'php_functions_list')]
+    public function list(PhpFunctionRepository $phpFunctionRepository): Response
+    {
+        $stringFunctions = $phpFunctionRepository->findByCategoryName('string');
+        $arrayFunctions = $phpFunctionRepository->findByCategoryName('array');
+        $fileFunctions = $phpFunctionRepository->findByCategoryName('file');
+        $dateFunctions = $phpFunctionRepository->findByCategoryName('date');
+        $mathsFunctions = $phpFunctionRepository->findByCategoryName('maths');
+        $errorFunctions = $phpFunctionRepository->findByCategoryName('error');
+        $jsonFunctions = $phpFunctionRepository->findByCategoryName('json');
+        $objectFunctions = $phpFunctionRepository->findByCategoryName('object');
+        $variableFunctions = $phpFunctionRepository->findByCategoryName('variable');
+        $xmlFunctions = $phpFunctionRepository->findByCategoryName('xml');
+        $mysqlFunctions = $phpFunctionRepository->findByCategoryName('mysql');
+        $cryptographyFunctions = $phpFunctionRepository->findByCategoryName('cryptography');
+        $curlFunctions = $phpFunctionRepository->findByCategoryName('curl');
+        $typeFunctions = $phpFunctionRepository->findByCategoryName('type');
+        $headersHttpFunctions = $phpFunctionRepository->findByCategoryName('headers_http');
+        $redFunctions = $phpFunctionRepository->findByCategoryName('red');
+
+        return $this->render('php_function/functions-list.html.twig', [
+            'stringFunctions' => $stringFunctions,
+            'arrayFunctions' => $arrayFunctions,
+            'fileFunctions' => $fileFunctions,
+            'dateFunctions' => $dateFunctions,
+            'mathsFunctions' => $mathsFunctions,
+            'errorFunctions' => $errorFunctions,
+            'jsonFunctions' => $jsonFunctions,
+            'objectFunctions' => $objectFunctions,
+            'variableFunctions' => $variableFunctions,
+            'xmlFunctions' => $xmlFunctions,
+            'mysqlFunctions' => $mysqlFunctions,
+            'cryptographyFunctions' => $cryptographyFunctions,
+            'curlFunctions' => $curlFunctions,
+            'typeFunctions' => $typeFunctions,
+            'headersHttpFunctions' => $headersHttpFunctions,
+            'redFunctions' => $redFunctions,
+        ]);
     }
 }
